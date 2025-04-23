f8b45891d23b9941caa7db067208e084afb3334d
windme2 <windme2@gmail.com>
2025-04-23 23:40:35 +0700

    Updated models for detect Json Cycle

--- Models/User.cs
+++ Models/User.cs
@@ -6,9 +6,8 @@ public class User
     public int Id { get; set; }
     public string FullName { get; set; } = string.Empty;
     public int DepartmentId { get; set; }
+    public string DepartmentName { get; set; } = string.Empty;
 
     [JsonIgnore] // Detect JSON Cycle
     public Department? Department { get; set; }
-
-    public string? DepartmentName => Department?.Name;
 }
